7a6fe797abb0be81c0fe14bbf4b25ebb5ca72e95
Benedict Waiharo <benabana@live.com>
2020-06-03 19:09:52 -0400

    Complete HW3

--- example/models.py
+++ example/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
 class BakedGood(models.Model):
     name = models.CharField(max_length=64)
     desc = models.CharField(max_length=256)
     good_type = models.TextChoices('type', 'BAGEL BREAD COOKIE CAKE PRETZEL')
+    sTypes = models.CharField(blank=True, choices=good_type.choices, max_length=20)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     recipe = models.TextField()
